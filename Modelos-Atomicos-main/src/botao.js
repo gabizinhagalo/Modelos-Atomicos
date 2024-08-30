967dcc451847b0c8874cbb5224480d3098789665
Luis Brandes <luishfbrandes@gmail.com>
2024-08-29 22:10:14 -0300

    Complemento do responsive desing e organização das pastas e consertar o java script

--- Modelos-Atomicos-main/src/botao.js
+++ Modelos-Atomicos-main/src/botao.js
@@ -1,5 +1,5 @@
 let botao = document.querySelector('.logo')
 
 botao.addEventListener('click', function(){
-    window.location.assign("http://127.0.0.1:5500/index.html")
+    window.location.assign("index.html")
 })
